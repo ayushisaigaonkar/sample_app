efec82aa471f724a6e220b7032d9cde5ae49f623
ayushisaigaonkar <ayushi.saigaonkar@mirraw.com>
2022-07-05 17:20:41 +0530

    Finish static pages

---
+++ app/controllers/normal_pages_controller.rb
@@ -0,0 +1,2 @@
+class NormalPagesController < ApplicationController
+end
---
+++ test/controllers/normal_pages_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class NormalPagesControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
